b5e364e5caf1f8a7a90fd3686b40532d65924fc8
rahim firouzi <r.firouzi.work@gmail.com>
2016-12-31 11:17:17 +0330

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    });
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function(event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+        //$("#navbar").collapse('hide');
+    });
+});
